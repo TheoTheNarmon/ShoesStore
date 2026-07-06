b902da1eaca11baefdcca3abdbaf5b52cc94f5cc
Theo Narmontas <narmontastheo@gmail.com>
2026-07-06 17:01:52 -0300

    search

--- src/pages/menu/menu.jsx
+++ src/pages/menu/menu.jsx
@@ -1,13 +1,48 @@
+import { useState } from 'react'
 import Item from '../../components/item/item'
 import ItemsContainer from '../../components/itemsContainer/itemsContainer'
 import './menu.css'
 
 function Menu({products}) {
+  const [search, setSearch] = useState('');
+  const [items, setItems] = useState(products);
+
+  const handleSetSearch = (e) => {
+    setSearch(e.target.value);
+    if(search.trim() == '' || search == ' '){
+      setItems(products);
+      return;
+    }
+    handleSearch();
+  }
+
+  const handleSearch = () => {
+
+    setItems(products.filter((product) => {
+      const searchLower = search.trim().toLowerCase();
+      const nameLower = product.name.toLowerCase();
+      const brandLower = product.brand.toLowerCase();
+
+      return (nameLower.includes(searchLower));
+    }))
+  }
+
   return (
-    /*esta pagina tendra un buscador y un filtro */
     <div>
-      <h1>Encuentra tu producto ideal</h1> 
-      <ItemsContainer items={products}></ItemsContainer>
+      <h1 className='tittle-shop'>Encuentra tu producto ideal</h1>
+      <div className='search-container'>
+        <input
+          type='text'
+          placeholder='Buscador...'
+          value={search}
+          onChange={handleSetSearch}
+          className='shop-search'
+        />
+      </div> 
+      <ItemsContainer items={items}></ItemsContainer>
+      {!items && (
+        <h3>Ningun producto encontrado...</h3>
+      )}
     </div>
   )
 }
